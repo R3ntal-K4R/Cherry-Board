8748d4329299eaba02fd3fff83cde2a4fd3c811b
Sean CHen <Seanchendu@gmail.com>
2025-04-21 17:57:04 -0500

    hi:

---
+++ 01_Frontend/src/pages/landing.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Landing() {
+
+
+  
+  return (
+    <>
+      <nav className='' style={flex}>
+
+      </nav>
+
+    </>
+  );
+}
