ca1d38f3af33f7858d28b40ca2b548c61af3c379
Garrett Provence <111098601+GarrettProv@users.noreply.github.com>
2025-04-22 18:59:30 -0500

    dwadwa
    dwadwa

--- 01_Frontend/src/pages/landing.jsx
+++ 01_Frontend/src/pages/landing.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
+import React from 'react';
+import styles from '../Landing.module.css';
 
 export default function Landing() {
-
-
-  
   return (
-    <>
-    <h1>hihi</h1>
-      <nav className='' style={{}}>
-      </nav>
+    <div className={styles.landingPage}>
+      <header className={styles.header}>
+        <div className={styles.logo}>Our Logo</div>
+        <div className={styles.authButtons}>
+          <button className={styles.signin}>Sign In</button>
+          <button className={styles.signup}>Sign Up</button>
+        </div>
+      </header>
 
-    </>
+      <main className={styles.main}>
+        <section className={styles.hero}>
+          <h1>
+            Keyboards Designed For <span className={styles.highlight}>You</span>
+          </h1>
+          <button className={styles.getStarted}>Get started</button>
+        </section>
+
+        <section className={styles.keyboardImage}>
+          <img src="keyboard_picture.webp" alt="Custom Keyboard" />
+        </section>
+      </main>
+    </div>
   );
 }
